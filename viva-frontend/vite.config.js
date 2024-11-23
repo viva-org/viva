1c70d2ac0832ccdc744e52260d04f1ad31015a67
Luis <liuysh20@gmail.com>
2024-11-23 21:13:32 +0800

    ci: 前后端统一在一个 VCS

---
+++ viva-frontend/vite.config.js
@@ -0,0 +1,27 @@
+// vite.config.js
+import { defineConfig } from 'vite' // 必须导入 defineConfig
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dts: 'src/components.d.ts',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})

59816cdbf858e9284f0b8dd4f60b7fccc730ae8b
Luis <liuysh20@gmail.com>
2025-01-07 16:19:39 +0800

    ci: github actions test

--- viva-frontend/src/services/api.js
+++ viva-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useUserStore } from '@/stores/user';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://43.134.93.160:8000';
 
 const api = axios.create({
   baseURL: API_URL,
